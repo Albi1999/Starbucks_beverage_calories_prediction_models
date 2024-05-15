9ea4177371a4490e135157aded5088d488c7539c
Alberto Calabrese <albertocalabrese99@gmail.com>
2024-05-15 12:20:15 +0200

    Code cleaned and updated

--- Code/Main.R
+++ Code/Main.R
@@ -6,13 +6,13 @@ library("corrplot")
 
 # Load data ----
 
-## Cinema/area 2018-2022 ----
+## Cinema/area 2018-2022
 # This data are related to the number of total shows, entries and expenses for cinema divided for each area
 shows_cine_area <- read_excel("Data/Cinema by territorial area.xlsx", sheet = 1)
 entries_cine_area <- read_excel("Data/Cinema by territorial area.xlsx", sheet = 2)
 expense_cine_area <- read_excel("Data/Cinema by territorial area.xlsx", sheet = 3)
 
-## Places/area 2018-2022 ----
+## Places/area 2018-2022
 # This data are related to the number of entertainment places for each area divided by activity
 pla_act_cin_area <- read_excel("Data/Places of performance in the regions.xlsx", sheet = 1)
 pla_act_tea_area <- read_excel("Data/Places of performance in the regions.xlsx", sheet = 2)
@@ -22,31 +22,31 @@ pla_act_bal_area <- read_excel("Data/Places of performance in the regions.xlsx",
 pla_act_via_area <- read_excel("Data/Places of performance in the regions.xlsx", sheet = 6)
 pla_act_mos_area <- read_excel("Data/Places of performance in the regions.xlsx", sheet = 7)
 
-## Total/area 2018-2022 ----
+## Total/area 2018-2022
 # This data are related to the number of total shows, entries and expenses for each area
 shows_area <- read_excel("Data/National territory all sectors (macro area and region).xlsx", sheet = 1)
 entries_area <- read_excel("Data/National territory all sectors (macro area and region).xlsx", sheet = 2)
 expense_area <- read_excel("Data/National territory all sectors (macro area and region).xlsx", sheet = 3)
 
-## Total/activity 2018-2022 ----
+## Total/activity 2018-2022
 # This data are related to the number of total shows, entries and expenses divided by activity
 shows_activity <- read_excel("Data/National territory all sectors (total).xlsx", sheet = 1)
 entries_activity <- read_excel("Data/National territory all sectors (total).xlsx", sheet = 2)
 expense_activity <- read_excel("Data/National territory all sectors (total).xlsx", sheet = 3)
 
-## Theatre/month 2018-2022 ----
+## Theatre/month 2018-2022
 # This data are related to the number of total shows, entries and expenses for theatral activity divided by month
 theatre_month <- read.csv("Data/Monthly trends by sector (Theater).csv", sep = ";", header = TRUE)
 
-## Cinema/month 2018-2022 ----
+## Cinema/month 2018-2022
 # This data are related to the number of total shows, entries and expenses for Cinema activity divided by month
 cinema_month <- read.csv("Data/Monthly trends by sector (Cinema).csv", sep = ";", header = TRUE)
 
-## Total/month 2018-2022 ----
+## Total/month 2018-2022
 # This data are related to the number of total shows, entries and expenses for the all the activities divided by month
 total_month <- read.csv("Data/Monthly trends by sector (All sectors).csv", sep = ";", header = TRUE)
 
-## Organizers/area 2018-2022 ----
+## Organizers/area 2018-2022
 # This data are related to the number of organizers for each area divided by activity
 org_act_cin_area <- read_excel("Data/Number of show organizers in the regions.xlsx", sheet = 1)
 org_act_tea_area <- read_excel("Data/Number of show organizers in the regions.xlsx", sheet = 2)
@@ -58,7 +58,7 @@ org_act_mos_area <- read_excel("Data/Number of show organizers in the regions.xl
 
 
 
-# What we have to do:
+# What we have to do: ----
 
 # EDA
 # 1. Understand the data structure
@@ -74,7 +74,7 @@ org_act_mos_area <- read_excel("Data/Number of show organizers in the regions.xl
 # 7. Understand the correlation between the places and the activities
 # 8. Understand the correlation between the places and the entries (spettatori)
 # 9. Understand the correlation between the expenses and entries
-# 10. Understand the correlation between the number shows and ????
+# 10. Understand the correlation between the number shows and entries
 # 11. Understand the correlation between the different macro areas
 
 # MODELS
@@ -101,9 +101,11 @@ org_act_mos_area <- read_excel("Data/Number of show organizers in the regions.xl
 # il rapporto spesa spettatori è maggiore della media
 
 
+
 # Esplorative data analysis ----
 # This part is dedicated to the explorative data analysis of the data loaded above
 # The aim is to understand the data structure and the main characteristics of the data
+
 ## 1. Understand the data structure ----
 # Summary of the models to understand the data structure
 summary(shows_cine_area)
@@ -133,48 +135,31 @@ summary(org_act_bal_area)
 summary(org_act_via_area)
 summary(org_act_mos_area)
 
-## 2. Understand the main characteristics of the data ----
-# Data transformation to understand the main characteristics of the data
-total_month$Spesa.del.pubblico <- as.numeric(gsub(",", ".", total_month$Spesa.del.pubblico))
-cinema_month$Spesa.del.pubblico <- as.numeric(gsub(",", ".", cinema_month$Spesa.del.pubblico))
-theatre_month$Spesa.del.pubblico <- as.numeric(gsub(",", ".", theatre_month$Spesa.del.pubblico))
+## 2. Data Transformation ----
+total_month$Audience.spending <- as.numeric(gsub(",", ".", total_month$Audience.spending))
+cinema_month$Audience.spending <- as.numeric(gsub(",", ".", cinema_month$Audience.spending))
+theatre_month$Audience.spending <- as.numeric(gsub(",", ".", theatre_month$Audience.spending))
 
-## NA values in the data ----
+### NA values in the data ----
 # Check if there are NA values in the data
 is.na(total_month)
 is.na(cinema_month)
 is.na(theatre_month)
 
 # Since we have some NA in the monthly data
-# We have to remove them from the data to avoid problems in the analysis by sobstituting them with the regression estimation of the column.
-for (i in 2:ncol(total_month)) {
-  total_month[,i][is.na(total_month[,i])] <- lm(total_month[,i] ~ total_month$Numero.spettacoli, 
-                                                data = total_month)$fitted.values
-}
+# We have to remove them from the data to avoid problems in the analysis 
+# by sobstituting them with the regression estimation of the column.
 
 for (i in 2:ncol(total_month)) {
-  total_month[,i][is.na(total_month[,i])] <- lm(total_month[,i] ~ total_month$Ingressi, 
-                                                data = total_month)$fitted.values
+  total_month[,i][is.na(total_month[,i])] <- mean(total_month[,i], na.rm = TRUE)
 }
 
 for (i in 2:ncol(cinema_month)) {
-  cinema_month[,i][is.na(cinema_month[,i])] <- lm(cinema_month[,i] ~ cinema_month$Numero.spettacoli, 
-                                                  data = cinema_month)$fitted.values
-}
-
-for (i in 2:ncol(cinema_month)) {
-  cinema_month[,i][is.na(cinema_month[,i])] <- lm(cinema_month[,i] ~ cinema_month$Ingressi, 
-                                                  data = cinema_month)$fitted.values
+  cinema_month[,i][is.na(cinema_month[,i])] <- mean(cinema_month[,i], na.rm = TRUE)
 }
 
 for (i in 2:ncol(theatre_month)) {
-  theatre_month[,i][is.na(theatre_month[,i])] <- lm(theatre_month[,i] ~ theatre_month$Numero.spettacoli, 
-                                                    data = theatre_month)$fitted.values
-}
-
-for (i in 2:ncol(theatre_month)) {
-  theatre_month[,i][is.na(theatre_month[,i])] <- lm(theatre_month[,i] ~ theatre_month$Ingressi, 
-                                                    data = theatre_month)$fitted.values
+  theatre_month[,i][is.na(theatre_month[,i])] <- mean(theatre_month[,i], na.rm = TRUE)
 }
 
 # Check if the NA values have been removed
@@ -184,69 +169,101 @@ is.na(theatre_month)
 # Ok, no more NA
 
 
-# My part: ------
+# Albi's Part: ------
 # the task is understand the correlation between the expenses and entries
 # The data are entries_area, entries_cine_area, entries_theatre_area, expense_area, expense_cine_area, expense_theatre_area
 # Create the code to do it
 
 # Calculate correlation between entries and expenses for each area
 
-# Transform the data by inverting the rows and columns
+# Correlation expense - entries ----
+# Understand the correlation between the expenses and entries
 
-# Transpose the data frame
-entries_activity_t <- as.data.frame(t(entries_activity))
+## Print the correlation coefficients ----
+print(paste("Correlation between cinema entries and cinema expenses: ", 
+            cor(expense_activity$`Cinematographic activity`, entries_activity$`Cinematographic activity`)))
+print(paste("Correlation between theatre entries and theatre expenses: ", 
+            cor(expense_activity$`Theatrical activity`, entries_activity$`Theatrical activity`)))
+print(paste("Correlation between concert entries and concert expenses: ", 
+            cor(expense_activity$`Concert activity`, entries_activity$`Concert activity`)))
+print(paste("Correlation between sport entries and sport expenses: ", 
+            cor(expense_activity$`Sports activity`, entries_activity$`Sports activity`)))
+print(paste("Correlation between dance entries and dance expenses: ", 
+            cor(expense_activity$`Dance activities and concerts`, entries_activity$`Dance activities and concerts`)))
+print(paste("Correlation between travelling show entries and travelling show expenses: ", 
+            cor(expense_activity$`Attractions of the travelling show`, entries_activity$`Attractions of the travelling show`)))
+print(paste("Correlation between exhibitions entries and exhibitions expenses: ", 
+            cor(expense_activity$Exhibitions, entries_activity$Exhibitions)))
 
-# Set the column names of the transposed data frame to the elements in the first column
-colnames(entries_activity_t) <- entries_activity_t[1, ]
+## Plot the results with corrplot ----
 
-# Remove the first row of the transposed data frame
-entries_activity_t <- entries_activity_t[-1, ]
+cor_expense_entries <- cor(expense_activity[-1,-1], entries_activity[-1,-1])
 
-# Transpose the data frame
-expense_activity_t <- as.data.frame(t(expense_activity))
+corrplot(cor_expense_entries, method = "number", tl.col = "black", addCoef.col = "black", tl.srt = 45)
+text(x = 2, y = 10.5, labels = "Correlation between expenses and entries")
 
-# Set the column names of the transposed data frame to the elements in the first column
-colnames(expense_activity_t) <- expense_activity_t[1, ]
+# No more useful
+# corrplot(cor(expense_activity[-1,-1], entries_activity[-1,-1]), method = "number",
+#         type = "upper", tl.col = "black", tl.srt = 90, diag = TRUE,
+#          addCoef.col = "black", addCoefasPercent = TRUE)
 
-# Remove the first row of the transposed data frame
-expense_activity_t <- expense_activity_t[-1, ]
+# Correlation shows - entries ----
+# Understand the correlation between the number shows and entries
+## Print the correlation coefficients ----
 
-# Transform in numeric boh the data set
-expense_activity_t <- as.data.frame(sapply(expense_activity_t, as.numeric))
-entries_activity_t <- as.data.frame(sapply(entries_activity_t, as.numeric))
+print(paste("Correlation between cinema shows and cinema entries: ", 
+            cor(shows_activity$`Cinematographic activity`, entries_activity$`Cinematographic activity`)))
+print(paste("Correlation between theatre shows and theatre entries: ", 
+            cor(shows_activity$`Theatrical activity`, entries_activity$`Theatrical activity`)))
+print(paste("Correlation between concert shows and concert entries: ", 
+            cor(shows_activity$`Concert activity`, entries_activity$`Concert activity`)))
+print(paste("Correlation between sport shows and sport entries: ", 
+            cor(shows_activity$`Sports activity`, entries_activity$`Sports activity`)))
+print(paste("Correlation between dance shows and dance entries: ", 
+            cor(shows_activity$`Dance activities and concerts`, entries_activity$`Dance activities and concerts`)))
+print(paste("Correlation between travelling show shows and travelling show entries: ", 
+            cor(shows_activity$`Attractions of the travelling show`, entries_activity$`Attractions of the travelling show`)))
+print(paste("Correlation between exhibitions shows and exhibitions entries: ", 
+            cor(shows_activity$Exhibitions, entries_activity$Exhibitions)))
 
+## Plot the results with corrplot ----
 
-cor_entries_expense_cine <- cor(expense_activity_t$"Attività cinematografica", entries_activity_t$"Attività cinematografica")
-cor_entries_expense_thea <- cor(expense_activity_t$"Attività teatrale", entries_activity_t$"Attività teatrale")
-cor_entries_expense_con <- cor(expense_activity_t$"Attività concertistica", entries_activity_t$"Attività concertistica")
-cor_entries_expense_spo <- cor(expense_activity_t$"Attività sportiva", entries_activity_t$"Attività sportiva")
-cor_entries_expense_bal <- cor(expense_activity_t$`Attività di ballo e concertini`, entries_activity_t$`Attività di ballo e concertini`)
-cor_entries_expense_via <- cor(expense_activity_t$"Attrazioni dello spettacolo viaggiante", entries_activity_t$"Attrazioni dello spettacolo viaggiante")
-cor_entries_expense_mos <- cor(expense_activity_t$"Mostre ed esposizioni", entries_activity_t$"Mostre ed esposizioni")
+cor_shows_entries <- cor(shows_activity[-1,-1], entries_activity[-1,-1])
 
-# Print the correlation coefficients
-print(paste("Correlation between cinema entries and cinema expenses: ", cor_entries_expense_cine))
-print(paste("Correlation between theatre entries and theatre expenses: ", cor_entries_expense_thea))
-print(paste("Correlation between concert entries and concert expenses: ", cor_entries_expense_con))
-print(paste("Correlation between sport entries and sport expenses: ", cor_entries_expense_spo))
-print(paste("Correlation between dance entries and dance expenses: ", cor_entries_expense_bal))
-print(paste("Correlation between travelling show entries and travelling show expenses: ", cor_entries_expense_via))
-print(paste("Correlation between exhibitions entries and exhibitions expenses: ", cor_entries_expense_mos))
-
-# Plot the results with corrplot ----
-# Load the library
-corrplot(cor(expense_activity_t, entries_activity_t), method = "number")
+corrplot(cor_shows_entries, method = "number", tl.col = "black", addCoef.col = "black", tl.srt = 45)
+text(x = 2, y = 10.5, labels = "Correlation between shows and entries")
 
 
+# Correlation places - expense ----
 
-corrplot(cor(expense_activity_t, entries_activity_t), method = "number", type = "upper", tl.col = "black", tl.srt = 45, diag = FALSE, addCoef.col = "black", addCoefasPercent = TRUE)
+# Understand the correlation between the places and the expenses
+
+## Print the correlation coefficients ----
+
+print(paste("Correlation between cinema places and cinema expenses: ", 
+            cor(pla_act_cin_area$'2022', expense_area$'2022')))
+print(paste("Correlation between cinema places and cinema expenses: ", 
+            cor(pla_act_cin_area$'2021', expense_area$'2021')))
+print(paste("Correlation between cinema places and cinema expenses: ",
+            cor(pla_act_cin_area$'2020', expense_area$'2020')))
+print(paste("Correlation between cinema places and cinema expenses: ",
+            cor(pla_act_cin_area$'2019', expense_area$'2019')))
+print(paste("Correlation between cinema places and cinema expenses: ",
+            cor(pla_act_cin_area$'2018', expense_area$'2018')))
 
 
+cor_cin_expence_area <- cor(pla_act_cin_area[-1,-1], expense_area[-1,-1])
+
+corrplot(cor_cin_expence_area, method = "number", tl.col = "black", addCoef.col = "black", tl.srt = 45)
+text(x = 2, y = 10.5, labels = "Correlation between places and expenses")
 
 
-# Understand the correlation between the number shows and ????
-# Understand the correlation between the different macro areas
+# Covariance matrix ----
+# Understand the covariance matrix between the different areas
 
+## Print the covariance matrix ----
 
-
+cov(entries_area[-1,-1])
+cov(expense_area[-1,-1])
+cov(shows_area[-1,-1])
 
